809a74c9a5670a3f47841126877a9cefbf304ff1
Billy Hnath <bhnath@brightcove.com>
2013-12-19 18:14:49 -0500

    initial import ais sample app

---
+++ BasicAkamaiIdentityServiceSampleApp/src/main/java/com/brightcove/player/samples/ais/basic/MainActivity.java
@@ -0,0 +1,54 @@
+package com.brightcove.player.samples.ais.basic;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import com.brightcove.player.event.EventEmitter;
+import com.brightcove.player.event.EventLogger;
+import com.brightcove.player.media.Catalog;
+import com.brightcove.player.media.VideoListener;
+import com.brightcove.player.model.Video;
+import com.brightcove.player.view.BrightcovePlayer;
+import com.brightcove.player.view.BrightcoveVideoView;
+
+/**
+ * This app illustrates how to use the Akamai Identity Service Plugin with the
+ * Brightcove Player for Android.
+ *
+ * @author Billy Hnath
+ */
+
+public class MainActivity extends BrightcovePlayer {
+
+    public static final String TAG = MainActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // Establish the video object and use it's event emitter to get important notifications
+        // and to control logging and media.
+        setContentView(R.layout.ais_activity_main);
+        brightcoveVideoView = (BrightcoveVideoView) findViewById(R.id.brightcove_video_view);
+        super.onCreate(savedInstanceState);
+        EventEmitter eventEmitter = brightcoveVideoView.getEventEmitter();
+        new EventLogger(eventEmitter, true, getClass().getSimpleName());
+
+
+
+        // Create the catalog object which will start and play the video.
+        Catalog catalog = new Catalog("FqicLlYykdimMML7pj65Gi8IHl8EVReWMJh6rLDcTjTMqdb5ay_xFA..");
+        catalog.findVideoByID("2142125168001", new VideoListener() {
+
+            @Override
+            public void onError(String error) {
+                Log.e(TAG, error);
+            }
+
+            @Override
+            public void onVideo(Video video) {
+                brightcoveVideoView.add(video);
+                brightcoveVideoView.start();
+            }
+        });
+    }
+
+}
