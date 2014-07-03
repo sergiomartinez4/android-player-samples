d1cf5cea18cc7b8812157ba66b940e4251545c63
Bryan Scott <pmreilly@brightcove.com>
2014-07-03 10:30:34 -0400

    Beginning work on Replay.java
    ##Changed Files
    Replay.java
    **NOTE: This test in its current form will take up more than six minutes as it runs through the entire video for both test methods. When AND-13 is resolved, this process will be expedited somewhat.**
    * Reordered methods to fit traditional pattern
    * renamed firstTimeWatch to setUpReplay as it now includes initializing the second play.
    * replaced seekTo utility method with skipAhead utility method, as working with the fast forward button is much easier than working with the seek bar.
      * this may be added to the superclass later, if it is useful for other tests and stable enough
      * for now, the purpose of skipAhead is defeated by the bug currently on Jira under AND-13.
        * skipAhead and setUpReplay have been altered (with the originals in comments) to still function as much as possible without being able to accurately skip through content blocks.
    * added javadoc comments

--- BasicOnceUxSampleApp/src/uiTest/java/com/brightcove/player/samples/onceux/basic/test/Replay.java
+++ BasicOnceUxSampleApp/src/uiTest/java/com/brightcove/player/samples/onceux/basic/test/Replay.java
@@ -1,42 +1,133 @@
 package com.brightcove.player.samples.onceux.basic.test;
 
+import java.util.concurrent.TimeUnit;
+
+import android.util.Log;
+
 import com.android.uiautomator.core.UiObject;
 import com.android.uiautomator.core.UiObjectNotFoundException;
+import com.android.uiautomator.core.UiDevice;
 import com.android.uiautomator.core.UiScrollable;
 import com.android.uiautomator.core.UiSelector;
+import com.android.uiautomator.core.UiWatcher;
 import com.android.uiautomator.testrunner.UiAutomatorTestCase;
 
 /**
- * Provides a class to test if the Sample App's replay functionality 
- * works correctly, in terms of actually playing over from the 
- * beginning after play has stopped entirely.
+ * Provides a class to test if the Sample App's replay functionality works correctly, in terms 
+ * of actually playing over from the beginning after play has stopped entirely. It also checks
+ * that ads can be skipped.
  */
-public class Replay extends UiAutomatorTestCase {
+public class Replay extends OnceUxUiAutomatorBaseTestCase {
 
-    //For Logcat.
+    /**
+     * The Android logcat tag.
+     */
     private final String TAG = this.getClass().getSimpleName();
+
+
+    // Test Methods
+    /**
+     * testReplay checks if the replay functionality of the Sample App is functional. It moves
+     * through a first "viewing" of the video with setUpReplay, then waits a moment and begins
+     * play again. After it begins play, it waits another few seconds, then checks the player
+     * has progressed the correct number of seconds.
+     */
+    public void testReplay() throws Exception {
+        Log.v(TAG, "Beginning testReplay");
+        setUpReplay();
+        TimeUnit.SECONDS.sleep(5);
+        UiObject currentTimeUiObject = new UiObject(new UiSelector().resourceId("android:id/time_current"));
+        String currentTimeString;
+        try {
+            super.seekControls();
+            Log.v(TAG, "Getting current time.");
+            currentTimeString = currentTimeUiObject.getText();
+        } catch (UiObjectNotFoundException currentTimeNotFound) {
+            Log.v(TAG, "Current time not found. Retrying...");
+            super.seekControls();
+            currentTimeString = currentTimeUiObject.getText();
+            Log.v(TAG, "Current time is " + currentTimeString);
+        }
+        assertTrue("Incorrect time elapsed.", currentTimeString.equals("00:05"));
+        Log.v(TAG, "Finished testReplay");
+
+    }
+
+    /**
+     * testReplayCheckAdBreaks checks for ad breaks during a Sample app's replay of the video.
+     * If any ad breaks are found, the test will fail. The test defines, registers, and runs a 
+     * UiWatcher that watches for a UiObject whose text begins with "Your video will resume in"
+     * and fails if it is present. This object is present in Ad Breaks and only ad breaks.
+     */
+    public void testReplayCheckAdBreaks() throws Exception {
+        Log.v(TAG, "Beginning testReplayCheckAdBreaks");
+        setUpReplay();
+
+        TimeUnit.SECONDS.sleep(5);
+        UiObject adMarkerText = new UiObject(new UiSelector().textStartsWith("Your video will resume in "));
+        if(adMarkerText.exists()) {
+            Log.v(TAG, "Ad Marker Text exists.");
+        } else {
+            Log.v(TAG, "Ad Marker Text not visible.");
+        }
+        assertFalse("Failure: Ad Break Found.", (adMarkerText.waitForExists(90000)));
+        Log.v(TAG, "Finished testReplayCheckAdBreaks");
+    }
 
     // Utility Methods
 
-    private void seekTo() {
-        // Used to seek to specific locations to expedite testing by skipping content. To be further defined using the UiAutomator framework.
-    }
-    private void firstTimeWatch() {
-        // To be called in testReplay.
-        //Steps: 
-        // Wait for ads to load and prerolls to complete
-        // seekTo the end of the content
-        // Wait for final EndAdBreak event
-        // Wait for stop/endContent event
+  /**
+     * skipAhead uses the UiAutomator API to press the fast forward button a number of times,
+     * based on the input number. It takes the number of seconds, divided by how many seconds
+     * a single press of the fast forward button moves, then (after rounding down that number)
+     * presses the fast forward button the number of times calculated.
+     *
+     * Unfortunately, due to the nature of the Sample App as it is now, fast forwarding will often
+     * get caught in a loop and end up 30 seconds or even a full minute backward, criplling its
+     * functionality. As a result, until this is fixed, skipAhead will print out the information
+     * that has been specified, but not do any actual fast forwarding.
+     *
+     * @throws UiObjecNotFoundException if called within an ad block, where the fast forward
+     * button does not exist.
+     */
+    private void skipAhead(int secondsValue) throws Exception {
+        Log.v(TAG, "Fast forwarding " + secondsValue + " seconds.");
+        UiObject ffwdButton = new UiObject(new UiSelector().resourceId("android:id/ffwd"));
+        int ffwdSecondsValue = 15;
+        // Cast to a double for the floating point divison, then recast to an int to round it 
+        // down to a whole number and so the for parameter is comparing two numbers of the same type.
+        double result = (double) secondsValue / ffwdSecondsValue;
+        int r = (int) result;
+        Log.v(TAG, "Fast forwarding " + secondsValue + " seconds requires " + result + " presses of the fast forward button. The button should be pressed " + r + " times.");
+        /* This has been commented out due to a bug that makes fast forwarding impossible.
+        for (int i = 1; i < r; i++) {
+            ffwdButton.click();
+            Log.v(TAG, "Fast forwarding. Number of fast forwards: " + i);
+        }
+        */
     }
 
-    // Test Methods
-    public void testReplay() throws UiObjectNotFoundException {
-        //Steps:
-        // Press Play to restart play
-        // if the play function does not work, or play resumes from somewhere that is not the beginning of the video, fail.
-        // test that ads are skippable
-        // if the seek controls are hidden, fail.
+    /**
+     * setUpReplay programmatically moves through the video to help expedite the process. It calls upon
+     * the skipAhead utility method to skip content blocks and waits through ad blocks.
+     */
+    private void setUpReplay() throws Exception {
+        super.playVideo();
+        
+        TimeUnit.SECONDS.sleep(200);
+        /* This has been commented out due to a bug that makes fast forwarding impossible.
+        TimeUnit.SECONDS.sleep(32);
+        skipAhead(30);
+        TimeUnit.SECONDS.sleep(30);
+        skipAhead(77);
+        TimeUnit.SECONDS.sleep(35);
+        */
+
+        // Due to the nature of the sample app, the first play after it has concluded serves as an
+        // alert that the video needs to reload. The second play serves to actually play.
+        super.playVideo();
+        super.playVideo();
+
     }
 
 }
