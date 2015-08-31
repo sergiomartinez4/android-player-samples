4bb2b7f0115197abdfdbf6fd23b9992aa588eb86
Paul Michael Reilly <pmreilly@brightcove.com>
2015-08-31 10:40:48 -0400

    Custom play button image
    <h1>Rationale:</h1>
    
    Some customers have been asking how to utilize a custom button image instead of the FontAwesome default choice.  A recent chnage to the SDK supports custom images via Brightcove attributes.  This commit provides a sample app to illustrate the steps involved.

---
+++ brightcove-player-controls/PlayImageButtonSampleApp/src/main/java/com/brightcove/player/demo/customplaybutton/MainActivity.java
@@ -0,0 +1,38 @@
+package com.brightcove.player.demo.customplaybutton;
+
+import com.brightcove.player.model.Video;
+import com.brightcove.player.view.BrightcovePlayer;
+import com.brightcove.player.view.BrightcoveVideoView;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+
+/**
+ * This app illustrates customizing the rewind button glyph using the Brightcove media controller.  The code for the
+ * rewind button glyph is a customizable resource.  See res/values/strings.xml for the gory detail.
+ *
+ * @author Paul Michael Reilly
+ */
+public class MainActivity extends BrightcovePlayer {
+
+    // Private class constants
+
+    private final String TAG = this.getClass().getSimpleName();
+
+    @Override protected void onCreate(Bundle savedInstanceState) {
+        // When extending the BrightcovePlayer, we must assign the BrightcoveVideoView before
+        // entering the superclass. This allows for some stock video player lifecycle
+        // management.  Establish the video object and use it's event emitter to get important
+        // notifications and to control logging.
+        setContentView(R.layout.default_activity_main);
+        brightcoveVideoView = (BrightcoveVideoView) findViewById(R.id.brightcove_video_view);
+        super.onCreate(savedInstanceState);
+
+        // Add a test video from the res/raw directory to the BrightcoveVideoView.
+        String PACKAGE_NAME = getApplicationContext().getPackageName();
+        Uri video = Uri.parse("android.resource://" + PACKAGE_NAME + "/" + R.raw.shark);
+        brightcoveVideoView.add(Video.createVideo(video.toString()));
+    }
+
+}
